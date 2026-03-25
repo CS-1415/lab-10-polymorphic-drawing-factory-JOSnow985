a478be154ff2078447466697048224f48baf0935
JOSnow985 <jaden.olvera@students.snow.edu>
2026-03-25 17:46:20 -0600

    Step 3: Create Factory Interface

---
+++ Lab09/IGraphic2DFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab09;
+
+// Represents a graphical element (i.e. a shape) that can be printed to the
+// console.
+public interface IGraphic2DFactory
+{
+    // A string describing what type of graphic will be built
+    public static readonly string Name = "";
+
+    // draws the graphic on the screen; return true if successful
+    bool Display();
+
+    // creates a new graphic object
+    IGraphic2D Create();
+}
