df901ab8aaee748d5a569b2ffc0a95cbe180ec72
JOSnow985 <jaden.olvera@students.snow.edu>
2026-03-25 17:55:32 -0600

    Main: Greeting

--- Lab09.Main/Program.cs
+++ Lab09.Main/Program.cs
@@ -1,4 +1,7 @@
 ﻿using Lab09;
 
+Console.Clear();
+Console.WriteLine("Welcome to a drawing program that will allow the user (that's you!) to draw or modify an existing drawing!");
+
 // List of instances of available factories, currently empty without concrete factories
 List<IGraphic2DFactory> availableShapeTypes = [];
