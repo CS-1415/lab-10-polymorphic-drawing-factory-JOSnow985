d16ddb9129493e89b1b6a4bb41d655c353e2dc6c
JOSnow985 <jaden.olvera@students.snow.edu>
2026-03-25 17:56:31 -0600

    Main: Built shapes list

--- Lab09.Main/Program.cs
+++ Lab09.Main/Program.cs
@@ -5,3 +5,6 @@ Console.WriteLine("Welcome to a drawing program that will allow the user (that's
 
 // List of instances of available factories, currently empty without concrete factories
 List<IGraphic2DFactory> availableShapeTypes = [];
+
+// List of shapes constituting the drawing
+List<IGraphic2D> builtShapes = [];
